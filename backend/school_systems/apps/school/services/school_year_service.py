86a8a87d06ef5f87af8137ced288d525ccfb6720
Mikhael <cayundamichael@gmail.com>
2026-05-24 17:54:30 +0800

    final season: part3

--- backend/school_systems/apps/school/services/school_year_service.py
+++ backend/school_systems/apps/school/services/school_year_service.py
@@ -3,21 +3,24 @@ from rest_framework.serializers import ValidationError
 
 from apps.school.models.school_year import SchoolYear
 
+from shared.utils.helper.school import get_user_school
+
 @transaction.atomic
-def create_school_year(*,school_staff_profile,school,validated_data):
+def create_school_year(*,school_staff_profile,name,start_date,end_date):
+    school = get_user_school(school_staff_profile)
     current_school_year = SchoolYear.objects.filter(school=school).order_by('-created_at').first()
     if current_school_year and current_school_year.is_current_school_year:
         raise ValidationError('Current school year is active, update it instead')
     
-    name = validated_data['name']
-    start_date = validated_data['start_date']
-    end_date = validated_data['end_date']
 
-    school_year = SchoolYear.objects.create(
+    school_year = SchoolYear(
         school=school,
         name=name,
         start_date=start_date,
         end_date=end_date,
     )
+
+    school_year.full_clean()
+    school_year.save()
     
     return school_year
--- backend/school_systems/apps/school/urls.py
+++ backend/school_systems/apps/school/urls.py
@@ -2,8 +2,15 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+from .api import views as school
+
 router = DefaultRouter()
 
+router.register(r'departments', school.DepartmentViewSet, basename='departments')
+router.register(r'courses', school.CourseViewSet, basename='courses')
+router.register(r'schools', school.SchoolListSerializer, basename='schools')
+router.register(r'school_year', school.SchoolYearViewSet, basename='school_year')
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
