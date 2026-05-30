fc0c94d72fd0dbd5b0c5ed98d5617c73ce3af3d6
Mikhael <cayundamichael@gmail.com>
2026-05-31 00:08:13 +0800

    time update

--- backend/school_systems/apps/election/api/v1/serializers/update.py
+++ backend/school_systems/apps/election/api/v1/serializers/update.py
@@ -5,4 +5,16 @@ from apps.election.models import Election
 class ElectionUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Election
-        fields = ['name','description','status']
+        fields = ['name','description','status','start_datetime','end_datetime']
+
+class ElectionUpdateTimeSerializer(serializers.Serializer):
+    start_datetime = serializers.DateTimeField()
+    end_datetime = serializers.DateTimeField()
+
+    def validate(self, attrs):
+        if attrs['start_datetime'] >= attrs['end_datetime']:
+            raise serializers.ValidationError(
+                'End datetime must be after start datetime'
+            )
+
+        return attrs
